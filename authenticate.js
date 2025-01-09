d6039dddb0515fc20b87a98880b2fac53e46229b
Rolex-arjun <143622488+Rolex-arjun@users.noreply.github.com>
2025-01-09 21:54:57 +0530

    Create authenticate.js

---
+++ authenticate.js
@@ -0,0 +1,17 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+
+const authenticate = async (req, res, next) => {
+  const token = req.header('x-auth-token');
+  if (!token) return res.status(401).send('Unauthorized');
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (err) {
+    res.status(400).send('Invalid Token');
+  }
+};
+
+module.exports = authenticate;
